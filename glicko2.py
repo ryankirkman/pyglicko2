146cca61b2f7929c39b374b228f7e9705f8a5124
Ryan Kirkman <ryan@ryankirkman.com>
2008-12-09 12:16:01 +1000

    Completed delta, g and E functions, as well as converted test values to Glicko2 scale.

--- glicko2.py
+++ glicko2.py
@@ -1,10 +1,19 @@
 import math
 
 class Player:
-    def __init__(self, rating = 1500, rd = 200):
+    def __init__(self, rating = 0, rd = 1.15129, vol = 0.06):
         # For testing purposes, preload the values assigned to an unrated player.
         self.__rating = rating
         self.__rd = rd
+        self.__vol = vol
+        
+        # The constant used to convert between the glicko and glicko2 scales
+        #self.__glicko2const = 173.7178
+        
+        # Convert to the glicko2 scale.
+        #self.__g2rating = (self.__rating - 1500) / self.__glicko2const
+        #self.__g2rd = self.__rd / self.__glicko2const
+        
         
     def __preRatingRD(self, t = 1, c = 63.2):
         """ Calculates and updates the player's rating deviation for the
@@ -48,31 +57,40 @@ class Player:
         # Update rating deviation.
         self.__rd = math.sqrt(1 / ((1 / math.pow(self.__rd, 2)) + (1 / d2)))
         
-    def __d2(self, rating_list, RD_list):
-        """ The d^2 function of the Glicko system.
+    def __delta(self, rating_list, RD_list, outcome_list):
+        """ The delta function of the Glicko2 system.
         
-        __d2(list[int], list[int]) -> float
+        __delta(list, list, list) -> float
+        """
+        tempSum = 0
+        for i in range(len(rating_list)):
+            tempSum += self.__g(RD_list[i]) * (outcome_list[i] - self.__E(rating_list[i], RD_list[i]))
+        return self.__v(rating_list, RD_list) * temp2
+        
+    def __v(self, rating_list, RD_list):
+        """ The v function of the Glicko2 system.
+        
+        __v(list[int], list[int]) -> float
         """
         tempSum = 0
         for i in range(len(rating_list)):
             tempE = self.__E(rating_list[i], RD_list[i])
             tempSum += math.pow(self.__g(RD_list[i]), 2) * tempE * (1 - tempE)
-        return 1 / (math.pow(self.__q(), 2) * tempSum)
+        return 1 / tempSum
         
     def __E(self, p2rating, p2RD):
         """ The Glicko E function.
         
         __E(int) -> float
         """
-        return 1 / (1 + math.pow(10, (-1 * self.__g(p2RD) * (self.__rating - p2rating) / 400)))
+        return 1 / (1 + math.exp(-1 * self.__g(p2RD) * (self.__rating - p2rating)))
         
     def __g(self, RD):
-        """ The Glicko g(RD) function.
+        """ The Glicko2 g(RD) function.
         
-        __gRD() -> float
+        __g() -> float
         """
-        return 1 / math.sqrt(1 + 3 * math.pow(self.__q(), 2) 
-            * math.pow(RD, 2) / math.pow(math.pi, 2))
+        return 1 / math.sqrt(1 + 3 * math.pow(RD, 2) / math.pow(math.pi, 2))
         
     def __q(self):
         """ The q constant of the Glicko system. 
@@ -87,7 +105,7 @@ class Player:
     def E(self, p2rating, p2RD):
         return self.__E(p2rating, p2RD)
         
-    def d2(self, rating_list = [], RD_list = []):
+    def v(self, rating_list = [], RD_list = []):
         return self.__d2(rating_list, RD_list)
         
     def rating(self):
@@ -118,6 +136,7 @@ Ryan = Player()
 # with outcomes 1, 0 and 0.
 print "Old Rating: " + str(Ryan.rating())
 print "Old Rating Deviation: " + str(Ryan.rd())
-Ryan.update_player([1400, 1550, 1700], [30, 100, 300], [1, 0, 0])
+Ryan.update_player([(x - 1500) / 173.7178 for x in [1400, 1550, 1700]],
+    [x / 173.7178 for x in [30, 100, 300]], [1, 0, 0])
 print "New Rating: " + str(Ryan.rating())
 print "New Rating Deviation: " + str(Ryan.rd())
